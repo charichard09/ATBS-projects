331c97de0d86d623a17a8e3843f0941cdba00dbf
Richard Cha <charichard09@gmail.com>
2021-04-28 13:31:19 -0700

    added get method testing

--- beautifulSoupTest/selectMethodTest.py
+++ beautifulSoupTest/selectMethodTest.py
@@ -2,13 +2,25 @@
 
 import bs4
 
-exampleFile = open("beautifulSoupTest\\html_example.html")
-exampleSoup = bs4.BeautifulSoup(exampleFile.read(), "html.parser")
-elems = exampleSoup.select("#author")
+#Trying new statement "with open(file.ext) as i"
+with open("beautifulSoupTest\\html_example.html") as exampleFile:
+    #exampleSoup = bs4.BeautifulSoup(exampleFile.read(), "html.parser"), no need to read() exampleFile, 
+    # bs4 can directly convert FileObject to BeautifulSoupObject
+    exampleSoup = bs4.BeautifulSoup(exampleFile, "html.parser")
+    elems = exampleSoup.select("#author")
 
-print(type(elems))
-print(len(elems))
-print(type(elems[0]))
-print(elems[0].getText())
-print(str(elems[0]))
-print(elems[0].attrs)
+    print(type(exampleFile))
+    print(type(elems))
+    print(len(elems))
+    print(type(elems[0]))
+    print(elems[0].getText())
+    print(str(elems[0]))
+    print(elems[0].attrs)
+
+#Get data from an element's attributes
+soup = bs4.BeautifulSoup(open("beautifulSoupTest\\html_example.html"), "html.parser")
+spanElem = soup.select("span")[0]
+print(str(spanElem))
+print(spanElem.get("id"))
+print(spanElem.get("some_nonexistent_addr") == None)
+print(spanElem.attrs)
