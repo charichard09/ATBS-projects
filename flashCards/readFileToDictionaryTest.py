308d24ec7cf1e3c650af15a1f743971e8cc4a860
Richard Cha <charichard09@gmail.com>
2021-06-04 13:30:24 -0700

    turned into function to be called by flashCards.py

--- flashCards/readFileToDictionaryTest.py
+++ flashCards/readFileToDictionaryTest.py
@@ -3,49 +3,58 @@
 
 import sys, os, re
 
-#os.path.dirname(sys.argv[0]) will return a string of readFileToDictionaryTest.py's directory path, which we assume read_to_dict.txt is also in
-read_to_dict_fo = open(os.path.dirname(sys.argv[0]) + "/read_to_dict.txt", 'r')
+def quiz_me():
+    #os.path.dirname(sys.argv[0]) will return a string of readFileToDictionaryTest.py's directory path, which we assume read_to_dict.txt is also in
+    read_to_dict_fo = open(os.path.dirname(sys.argv[0]) + "/read_to_dict.txt", 'r')
 
-read_to_dict_text = read_to_dict_fo.read()
+    read_to_dict_text = read_to_dict_fo.read()
 
-read_to_dict_fo.close()
+    read_to_dict_fo.close()
 
-#Create a regular expression to look for all current "Questions on x" and print to screen. 
-print("Current Glossary of Available Questions:")
-available_questions_regex = re.compile(r"Questions\son\s.*")
-available_questions_mo = available_questions_regex.findall(read_to_dict_text)
-print('\n'.join(available_questions_mo))
+    #Create a regular expression to look for all current "Questions on x" and print to screen. 
+    print("\nCurrent Glossary of Available Questions:")
+    available_questions_regex = re.compile(r"Questions\son\s.*")
+    available_questions_mo = available_questions_regex.findall(read_to_dict_text)
+    print('\n'.join(available_questions_mo))
 
-#Ask user input of what they'd like to be quizzed on from the options given.
+    #Ask user input of what they'd like to be quizzed on from the options given.
+    while True:
+        user_topic = input("\nWhat would you like to be quizzed on today? (CH1, Universal Python, etc.)\n")
+        if "Questions on " + user_topic in read_to_dict_text:
+            break
+
+    #Create a regular expression to isolate text inbetween string "Questions on {topic}" and "Questions on {following topic}"
+    quest_ans_regex = re.compile(r"Questions\son\s" + user_topic + r".*Questions\son\s", re.DOTALL)
+    quest_ans_mo = quest_ans_regex.search(read_to_dict_text)
+
+    #TODO: Assign all !Q. to a dictionary as keys with respective !A. as values
+    questions_regex = re.compile(r"!Q\.\s.*")
+    answers_regex = re.compile(r"!A\.\s.*")
+
+    questions_match = questions_regex.findall(quest_ans_mo.group())
+    answers_match = answers_regex.findall(quest_ans_mo.group())
+
+    quest_ans_dict = {}
+
+    for i in range(len(questions_match)):
+        quest_ans_dict[questions_match[i]] = answers_match[i]
+
+    #Ask a random question from quest_ans_dict (random key), let user input their answer, then print the answer (value) of question (key)
+    for j in range(len(quest_ans_dict)):
+        user_answer = input('\n' + list(quest_ans_dict.keys())[j] + "\n\n")
+        print('\n' + quest_ans_dict[list(quest_ans_dict.keys())[j]] + '\n')
+
+    #Exit prompt to user
+    print("That's all the questions. Great job!")
+
+#Add option to be quizzed again
 while True:
-    user_topic = input("\nWhat would you like to be quizzed on today? (CH1, Universal Python, etc.)\n")
-    if "Questions on " + user_topic in read_to_dict_text:
+    quiz_me()
+    answer = input("Would you like to be quizzed again, or on another topic? (y/n)\n")
+    if answer == 'y':
+        continue
+    elif answer == 'n':
         break
-
-#Create a regular expression to isolate text inbetween string "Questions on {topic}" and "Questions on {following topic}"
-quest_ans_regex = re.compile(r"Questions\son\s" + user_topic + r".*Questions\son\s", re.DOTALL)
-quest_ans_mo = quest_ans_regex.search(read_to_dict_text)
-print(quest_ans_mo.group())
-
-#TODO: Assign all !Q. to a dictionary as keys with respective !A. as values
-questions_regex = re.compile(r"!Q\.\s.*")
-answers_regex = re.compile(r"!A\.\s.*")
-
-questions_match = questions_regex.findall(quest_ans_mo.group())
-answers_match = answers_regex.findall(quest_ans_mo.group())
-
-print(questions_match)
-print(answers_match)
-
-quest_ans_dict = {}
-
-for i in range(len(questions_match)):
-    quest_ans_dict[questions_match[i]] = answers_match[i]
-
-#Ask a random question from quest_ans_dict (random key), let user input their answer, then print the answer (value) of question (key)
-for j in range(len(quest_ans_dict)):
-    user_answer = input(list(quest_ans_dict.keys())[j] + "\n\n")
-    print('\n' + quest_ans_dict[list(quest_ans_dict.keys())[j]] + '\n')
-
-#Exit prompt to user
-print("That's all the questions. Great job!")
+    else:
+        print("     ERROR: invalid input")
+        break
