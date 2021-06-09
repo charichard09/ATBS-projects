9f4332eaba86fa4085b80c9043498690d9c41f8e
Richard Cha <charichard09@gmail.com>
2021-06-09 12:02:36 -0700

    first pseudo code created

---
+++ madLibs.py
@@ -0,0 +1,17 @@
+#Program that reads a text file and replaces the words ADJECTIVE, NOUN, ADVERB, or VERB with a prompted user input,
+#then create a new file with the new words in place of the old words and print to screen.
+#Date: 6-8-2021 #Dev: Richard Cha
+
+#TODO: open file as r+ and read to variable
+
+#TODO: create regex for ADJECTIVE, NOUN, ADVERB, VERB
+
+#TODO: split read string, iterate through list of all words and everytime 1 of 4 words comes up, 
+#prompt user to replace said word with input word, replace, continue
+# file_read.split(' '),
+
+#TODO: rejoin list with ' '.join(split_string) and print to screen
+
+
+
+
