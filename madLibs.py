7ec54149082a080885e722a2304e8c3e95afcd19
Richard Cha <charichard09@gmail.com>
2021-06-10 14:20:52 -0700

    fixed word placeholders /w punctation error

--- madLibs.py
+++ madLibs.py
@@ -2,7 +2,7 @@
 #then create a new file with the new words in place of the old words and print to screen.
 #Date: 6-8-2021 #Dev: Richard Cha
 
-import os, sys
+import os, sys, copy, re
 
 #open file as r+ and read to variable
 mad_libs_fo = open(os.path.dirname(sys.argv[0]) + "/madLibs.txt", 'r+')
@@ -17,10 +17,25 @@ mad_libs_list = mad_libs_text.split(' ')
 
 #TODO: split() method will split words with punctuation causing the program to not recognize words
 #Find out way to remove punctuation
+#Solution: remove punctation from words in mad_libs_list using regex and re.sub()
+ml_list_no_punc = copy.copy(mad_libs_list)
+
+#\W will match any character that is NOT a "word" character. That includes digits, letters, and spaces.
+punctuation_regex = re.compile(r"\W")
+
+for j in range(len(ml_list_no_punc)):
+    ml_list_no_punc[j] = punctuation_regex.sub('', ml_list_no_punc[j])
+
+
 for i in range(len(mad_libs_list)):
-    if mad_libs_list[i] in ["ADJECTIVE", "VERB", "NOUN", "ADVERB"]:
-        answer = input("Please enter a " + mad_libs_list[i].lower() + ":\n")
-        mad_libs_list[i] = answer
+    if ml_list_no_punc[i] in ["ADJECTIVE", "VERB", "NOUN", "ADVERB"]:
+        answer = input("Please enter a " + ml_list_no_punc[i].lower() + ":\n")
+        #Reattach punctation if punctation was found in original string containing ADJ, VER, NOU, ADV
+        if punctuation_regex.search(mad_libs_list[i]) != None:
+            punctuation_mo = punctuation_regex.search(mad_libs_list[i]) 
+            mad_libs_list[i] = answer + punctuation_mo.group()
+        else:
+            mad_libs_list[i] = answer 
 
 #rejoin list with ' '.join(split_string) and print to screen
 print(' '.join(mad_libs_list))
