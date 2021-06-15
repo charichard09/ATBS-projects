08525a15a91850787fe4910d254f8a9dccaead88
Richard Cha <charichard09@gmail.com>
2021-06-15 15:57:12 -0700

    started regexSearch and pseudocode

---
+++ regexSearch.py
@@ -0,0 +1,13 @@
+# Program that asks the user to input a string to search, then searches all the .txt files in a folder and returns 
+# the found string and the file the string was found in.
+
+import re
+
+# TODO: Have user input a string, then create regex from that string
+
+# TODO: Assume the text files are in a folder below this program called text2Search, search folder by writing a loop that will iterate through
+# the directory of text2Search, 
+
+# TODO: for each iteration, open .txt, save it, search it with regex_obj 
+# if regex_obj != NoneType, print regex_obj.group(), print i in iteration
+
