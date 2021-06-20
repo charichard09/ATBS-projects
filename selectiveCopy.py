c595f00b0f7bba3b46a346d9505040c3bdcd3ad3
Richard Cha <charichard09@gmail.com>
2021-06-20 13:41:35 -0700

    started pseudo code

---
+++ selectiveCopy.py
@@ -0,0 +1,12 @@
+# Program that walks a folder tree and copies all files of a certain extension (.jpeg, .txt, .html) to a new folder names "backup"
+
+# TODO: Print "What extension type will we be copying today?"
+# extension = input("")
+
+# TODO: Print "The current directory is _____" 
+# "Would you like to search the current directory? (y/n)"
+# if 'n' have user input the absolute address of the directory to be searched and copied
+
+# TODO: Walk directory and print all files and folders
+# create new folder (mkdir?) called "backup"
+# if extension in files string, copy it to new folder
