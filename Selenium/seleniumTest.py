84e83e080273801ffa080ff078b73f43738d7515
Richard Cha <charichard09@gmail.com>
2021-07-19 13:07:08 -0700

    testing click and read webElement obj

--- Selenium/seleniumTest.py
+++ Selenium/seleniumTest.py
@@ -2,7 +2,18 @@
 # Date: 07-17-21 Dev: Richard C.
 
 from selenium import webdriver
+import webbrowser
 
 PATH = r"C:\Users\chari\Documents\Python Programs\ATBS Scripts and Projects\Selenium\chromedriver.exe"
 browser = webdriver.Chrome(PATH)
 browser.get("http://inventwithpython.com")
+
+try:
+    link_elem = browser.find_element_by_link_text("Buy on Amazon")
+    #print("Found href to %s !" % (elem.get_attribute("href")))
+    link_elem.click()
+except:
+    print("Was not able to find an element with that name.")
+
+#webbrowser.open(elem.get_attribute("href"))
+#elem.click()
