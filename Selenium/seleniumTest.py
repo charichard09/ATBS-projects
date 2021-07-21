129f0188503a2361fde52c1f4865315de304b5fd
Richard Cha <charichard09@gmail.com>
2021-07-21 15:13:15 -0700

    converted click() to submit() method

--- Selenium/seleniumTest.py
+++ Selenium/seleniumTest.py
@@ -4,6 +4,8 @@
 from selenium import webdriver
 import webbrowser
 
+food = input("What food item would you like to look up today?\n")
+
 PATH = r"C:\Users\chari\Documents\Python Programs\ATBS Scripts and Projects\Selenium\chromedriver.exe"
 browser = webdriver.Chrome(PATH)
 browser.get("https://www.google.com")
@@ -11,9 +13,14 @@ browser.get("https://www.google.com")
 try:
     input_elem = browser.find_elements_by_tag_name("input")
     #print("Found href to %s !" % (elem.get_attribute("href")))
-    input_elem[0].send_keys("Pokemon Cards")
-    print(input_elem[3].get_attribute("value"))
-    input_elem[3].click()
+    input_elem[0].send_keys(food + " calories")
+
+    # Using submit() method to submit text in input element index 0
+    input_elem[0].submit()
+
+    # Can also use click() method on input element index 3 which is the Google Search button 
+    #print(input_elem[3].get_attribute("value"))
+    #input_elem[3].click()
 except:
     print("Was not able to find an element with that name.")
 
