8a390baa856ac8c5d3a6d2d89d9634f415ea8eac
Richard Cha <charichard09@gmail.com>
2021-08-16 04:23:32 -0700

    begin pseudo code for pomodoro program

---
+++ pomodoro.py
@@ -0,0 +1,12 @@
+# Pomodoro technique
+# Date: 8-16-2021 Dev: Richard C.
+
+import time
+
+# TODO: Ask user how long to set first pomo duration. Press Enter to then begin.
+
+# TODO: Use time.time() and execution time compensation to go through duration and print timer along the way.
+
+# TODO: When timer hits inputted duration, stop and sound an alarm (Lookup syntax for alarm sound)
+
+# TODO: Ask user to press ENTER when ready for 5 min interval. Reset loop (could also recall as function)
