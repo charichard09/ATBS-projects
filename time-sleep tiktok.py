0f87fc8b5f971b0d4bee08e35e1c58365f335ec0
Richard Cha <charichard09@gmail.com>
2021-08-16 04:22:45 -0700

    sleep module example from book

---
+++ time-sleep tiktok.py
@@ -0,0 +1,8 @@
+# Given program example to showcase the time.sleep() function from time module
+
+import time
+for i in range(3):
+    print("tik")
+    time.sleep(2)
+    print("tok")
+    time.sleep(2)
