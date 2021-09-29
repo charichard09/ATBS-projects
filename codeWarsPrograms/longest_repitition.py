175391cc87c57bdccb9f6459e5952e8bb382afe2
Richard Cha <charichard09@gmail.com>
2021-09-29 04:44:25 -0700

    added current work on longest rep

---
+++ codeWarsPrograms/longest_repitition.py
@@ -0,0 +1,37 @@
+# Program that will take a string as an argument and return the longest consecutive single character found and how many times it was found.
+# i.e. string "aaaabbaafff" would return ('a', 4)
+
+def longest_repetition(chars):
+
+    if chars == '':
+        return ('', 0)
+
+    char_count = {}
+
+    for i in set(chars):
+	    j = 1
+	
+	    # Check highest consecutive times char i shows up by increasing i by i each time
+	    while i * j in chars:
+		    j += 1
+	
+	    char_count[i] = j - 1
+    
+    answer = sorted(char_count.items(), key=lambda x:x[1], reverse=True)
+    
+    return answer[0]
+    
+    # Check if chars is blank, return ()'', 0)
+#    if chars == '':
+       # return ('', 0)
+    
+    # Iterate through string
+#    char_count = {}
+    
+#    for i in chars:
+#        char_count[i] = 1 + char_count.setdefault(i, 0)
+        
+        
+#    answer = sorted(char_count.items(), key=lambda x:x[1], reverse=True)
+    
+#    return answer[0]
