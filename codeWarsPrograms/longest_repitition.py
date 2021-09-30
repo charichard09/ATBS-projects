b67473270a04da6cf62b1944d3823572de38fa86
Richard Cha <charichard09@gmail.com>
2021-09-30 08:21:47 -0700

    learned n copied different approach

--- codeWarsPrograms/longest_repitition.py
+++ codeWarsPrograms/longest_repitition.py
@@ -4,22 +4,25 @@
 def longest_repetition(chars):
 
     if chars == '':
-        return ('', 0)
+        return print(str('', 0))
 
     char_count = {}
 
+    # set(chars) will eval to 1 of each possible char, then iterate for each character
     for i in set(chars):
 	    j = 1
 	
-	    # Check highest consecutive times char i shows up by increasing i by i each time
+	    # for every iteration, check if i + i is in the string {char}. Once i + i is not in string {char}, assign character i as i - 1,
+        # which is the highest consecutive amount of characters found in string {char}, to char_count key=i value=j-1
 	    while i * j in chars:
 		    j += 1
 	
 	    char_count[i] = j - 1
     
+    # Using sorted function, sort dictionary items in char_count from highest to lowest, then return highest being answer[0]
     answer = sorted(char_count.items(), key=lambda x:x[1], reverse=True)
     
-    return answer[0]
+    return print(answer[0])
 
     # Check if chars is blank, return ()'', 0)
     #    if chars == '':
@@ -30,6 +33,34 @@ def longest_repetition(chars):
     
     #    for i in chars:
     #        char_count[i] = 1 + char_count.setdefault(i, 0)
+        
+def longest_repetition2(chars):
+    # define a current max_count, max_char variable pair
+    # define a count, char, pair
+
+    max_count, max_char = 0, ''
+    count, char = 0, ''
+
+    # iterate through each letter of chars, for every new char, if the 
+    # same char, count + 1 and move to next char, if not same char,
+    # check if count is higher than max_count saved(first iteration where max_count
+    # is 0, count will always be higher and will auto assign first iterated char and count), if yes store assign 
+    # max_count to new count and max_char to new char, if not, continue to next char iteration, return max_char and max_count
+
+    for i in chars:
+        # If i (i.e. 'a') != char (i.e. 'b'), reassign char to i and count back to 0
+        if i != char:
+            char = i
+            count = 0
+        
+        # If i (i.e. 'a') == char (i.e. 'a'), increase count by 1
+        count += 1
+        
+        # If at any time count > max_count, assign max_count = count and max_char to char
+        if count > max_count:
+            max_count, max_char = count, char
+
+    return print(f"{max_char}, {str(max_count)}")
 
 # Test program
 tests = [
@@ -41,9 +72,6 @@ tests = [
     ["ba", ('b', 1)],
     ["", ('', 0)],
 ]
-longest_repetition(tests)
-        
-        
-#    answer = sorted(char_count.items(), key=lambda x:x[1], reverse=True)
-    
-#    return answer[0]
+for i in range(len(tests)):
+    longest_repetition2(tests[i][0])
+    print("Answer should be " + str(tests[i][1]))
