317a0f7a2cbed2554a52ebdfc041d9276d7eff0b
Richard Cha <charichard09@gmail.com>
2021-10-27 05:04:24 -0700

    begin pseudo coding

--- codeWarsPrograms/shift_13_letters.py
+++ codeWarsPrograms/shift_13_letters.py
@@ -2,3 +2,11 @@
 # the alphabet. Will ignore any symbols or numbers.
 # Date: 10/27/21 Dev: Richard C.
 
+import re
+
+# TODO: Create regex for simple aphabetical character
+
+# TODO: Problem: Any character at index[13] of the alphabet + 13 will exceed the alphabet.
+# Possible solution: create a string of 2 alphabets (i.e. new_string = "a-z" * 2)
+
+# TODO: Iterate through given string, each time checking regex_match_object to character. If match, possibly use regex_obj.sub() method?
