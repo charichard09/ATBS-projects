8eb5695971bb88d116034348305644a0809bc036
Richard Cha <charichard09@gmail.com>
2021-12-02 08:38:46 -0800

    added user input prompt

--- codeWarsPrograms/shift_13_letters.py
+++ codeWarsPrograms/shift_13_letters.py
@@ -29,11 +29,9 @@ def rot13(message):
     print(answer)
 
 # Test rot13
-rot13("""
-ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. ROT13 is an example of the Caesar cipher.
 
-Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
-""")
+response = input("Please enter a string you would like to shift:\n")
+rot13(response)
 
 
 
