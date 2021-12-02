9d9b1dca0e19cdb5683b336f01016318cee07e1c
Richard Cha <charichard09@gmail.com>
2021-12-02 08:05:14 -0800

    changed project descr to match solution

--- codeWarsPrograms/shift_13_letters.py
+++ codeWarsPrograms/shift_13_letters.py
@@ -1,4 +1,4 @@
-# Program (called ROT13 on Codewars) that will iterate through a string and replace any letter with the letter 13 places away from it in 
+# Program (called ROT13 on Codewars) that will iterate through a string and replace every 13th letter with the letter 13 places away from it in 
 # the alphabet. Will ignore any symbols or numbers.
 # Date: 10/27/21 Dev: Richard C.
 
