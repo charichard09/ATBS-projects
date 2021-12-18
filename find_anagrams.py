82322d7f9ee95de80f853a5f7bb2fd2c9eab3838
Richard Cha <charichard09@gmail.com>
2021-12-18 08:25:55 -0800

    finished and working

--- find_anagrams.py
+++ find_anagrams.py
@@ -3,21 +3,39 @@
 #  Date:  12-18-21    Dev: Richard Cha
 
 def anagrams(word, words):
-    #  TODO: if word is empty, return []
+    unique_word_letters = {}
+    unique_words_letters = {}
+    unique_character = ""
+    found_anagrams = []
 
+    #  if word is empty, return []
+    if len(word) == 0:
+        return found_anagrams
 
-    #  TODO: iterate through word and use count() function on each unique character and save in a dictionary. 
+    #  iterate through word and use count() function on each unique character and save in a dictionary. 
     #  for i in word:; i = unique_character; word.count(unique_character)
+    for letter in word:
+        if unique_character != letter:
+            unique_character = letter
+            unique_word_letters[unique_character] = word.count(unique_character)
+
+    #  iterate through the list words, for each iteration, count how many of each character is in the word and save to dictionary.
+    for word_in_list in words:
+        unique_character = ""
+        for letter in word_in_list:
+            if unique_character != letter:
+                unique_character = letter
+                unique_words_letters[unique_character] = word_in_list.count(unique_character)
+
+        #  compare both dictionaries, if they match, word is an anagram so save to a new list.
+        if unique_word_letters == unique_words_letters:
+            found_anagrams.append(word_in_list)
+        
+        unique_words_letters = {}
 
 
-    #  TODO: iterate through the list words, for each iteration, count how many of each character is in the word and save to dictionary.
-
-
-        #  TODO: compare both dictionaries, if they match, word is an anagram so save to a new list.
-
-
-    return words
+    return found_anagrams
 
 
 #  Test: answer should be ["aabb", "bbaa"]
-anagrams("abba", ["aabb", "abcd", "bbaa", "dada"])
+print(anagrams("ab", ["ab", "ba"]))
