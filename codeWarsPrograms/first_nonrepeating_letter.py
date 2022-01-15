f1094b814ba4231927c4e10d5726313333906631
Richard Cha <charichard09@gmail.com>
2022-01-15 06:31:29 -0800

    begin pseudo code

---
+++ codeWarsPrograms/first_nonrepeating_letter.py
@@ -0,0 +1,5 @@
+#  Write a program that when given a string will return the first nonrepeating letter of that string
+#  Date: 1-15-22 Dev: Richard C.
+
+def first_non_repeating_letter(string):
+    #your code here
