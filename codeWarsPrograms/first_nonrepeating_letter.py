f18c0b78da7e876974d08940645be61035bd0f3e
Richard Cha <charichard09@gmail.com>
2022-01-22 04:16:40 -0800

    finished first draft of revision

--- codeWarsPrograms/first_nonrepeating_letter.py
+++ codeWarsPrograms/first_nonrepeating_letter.py
@@ -2,20 +2,43 @@
 #  Date: 1-15-22 Dev: Richard C.
 
 def first_non_repeating_letter(word):
+    pass
     #  take string and iterate through each character, if character hasn't occurred, store character in dictionary and using count(), 
     #  attach the value of count
-    character_occurence = {}
+    character_occurrence = {}
+    word_ignorecase = word.lower()
+    upper_true = 0
 
-    for letter in word:
-        if letter not in character_occurence:
-            character_occurence[letter] = word.count(letter)
+    for i in range(len(word_ignorecase)):
+        if word_ignorecase[i] not in character_occurrence:
+            character_occurrence[word_ignorecase[i]] = word_ignorecase.count(word_ignorecase[i])
+
+            #  Add upper case check
+            if word[i].isupper():
+                upper_true = 1
 
     #  iterate through dictionary, on first instance of value 1, return as first nonrepeating letter of string
-    for key_letter in character_occurence:
-        if character_occurence[key_letter] == 1:
-            return key_letter
+    for key_letter in character_occurrence:
+        if character_occurrence[key_letter] == 1:
+            if upper_true:
+                return key_letter.upper()
+    
+    #  if no key:value equals 1, return "" aka nothing found
+    return ""
+
+
+#  Second attempt to shorten code
+def locate_non_repeat(characs):
+    character_occurrence = {}
+    for symbol in characs:
+        if symbol not in character_occurrence:
+            if symbol.isalpha():  
+                character_occurrence[symbol] = characs.count(symbol.lower()) + characs.count(symbol.upper())
+            else:
+                character_occurrence[symbol] = characs.count(symbol)
+
 
 #  Test
 test_strings = ["stress", "a", "moonmen", "abba", "", "~><#~><", "sTreSS"] 
 for i in test_strings:
-    print(first_non_repeating_letter(i))
+    print(locate_non_repeat(i))
