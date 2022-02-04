f6a3556ea9bfab570be09b49c965b4569ecea9a2
Richard Cha <charichard09@gmail.com>
2022-02-04 04:18:26 -0800

    begin pseudocode for string_to_int

---
+++ codeWarsPrograms/string_to_int.py
@@ -0,0 +1,16 @@
+#  Script that takes a string of a number and converts it to integer of that number.
+#  Minimum "zero" maximum "one million", must be able to handle "and" i.e. "one hundred and three"
+#  Date: 02-03-22 Dev: Richard C.
+
+
+def parse_int(string):
+    #  TODO: 
+     
+    return # number
+
+
+#  Test
+test = ["one", "twenty", "two hundred and forty-six", "seven hundred eighty-three thousand nine hundred and nineteen"]
+
+for string_of_number in test:
+    print(parse_int(string_of_number))
