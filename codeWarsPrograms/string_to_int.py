2e566782a46787202044d397651b8bf68c595f43
Richard Cha <charichard09@gmail.com>
2022-05-16 21:23:36 -0700

    halted to try again in pt 2

--- codeWarsPrograms/string_to_int.py
+++ codeWarsPrograms/string_to_int.py
@@ -7,18 +7,23 @@ def parse_int(int_word):
     #  Hypothesis: Figure out common patterns in written out numbers one through one million to simplify converting that string to int
     #  When solving for the final int number, anytime "and x" is found, the value of x comprises the final int number position
     #  i.e. "ten thousand and twenty" == 10x where x is 20
+    
+    print(f"The number we are parsing is: {int_word}")
 
+    #  Remove any hyphen characters and replace with ' '
+    int_word = int_word.replace('-', ' ')
+
+    #  Assign number to be stored and returned, and string broken into a list, to variables
     number = []
     int_word_seperated = int_word.split(" ")
-
 
     #  Make two dictionaries, one where keys are the strings of unique numbers and values are the int value of that number, and another 
     #  where keys are place value words and values are the length of given place value
 
     written_numbers = {"one":1, "two":2, "three":3, "four":4, "five":5, "six":6, "seven":7, "eight":8, "nine":9, "zero":0, "ten":10,
     "eleven":11, "twelve":12, "thirteen":13, "fourteen":14, "fifteen":15, "sixteen":16, "seventeen":17, "eighteen":18, "nineteen":19,
-    "twenty":20, "thirty":30, "fourty":40, "fifty":50, "sixty":60, "seventy":70, "eighty":80, "ninety":90}
-    place_value_length = {"hundred":00, "thousand":000, "million":000000}
+    "twenty":20, "thirty":30, "forty":40, "fifty":50, "sixty":60, "seventy":70, "eighty":80, "ninety":90}
+    place_value = {"hundred":100, "thousand":1000, "million":1000000}
 
     #  Use string.split(" ") to break string into a list of individual parts
 
@@ -31,11 +36,16 @@ def parse_int(int_word):
             number.append(written_numbers[word])
             continue
 
+        #  Check condition if item 1 in int_
+
         #  TODO: Figure out how to handle words like and, thousand, etc. that aren't unique numbers
+        #  Hypothesis: If I see "and", I can add the left and right side together. If I see a place value i.e. 100, I can multiply the left
+        #  value and place value to get the correct number.
         if word in written_numbers:
             number.append(written_numbers[word])
-        elif word in place_value_length:
-            number.append(place_value_length[word])
+        elif word in place_value:
+            for place_number in range(place_value[word]):
+                number.append(0)
 
     #  TODO: loop through new list of split string, for each item converting its string to its int equivalent, and putting into new string. If 
     #        string_of_number split is larger than 1 item, handle 2nd item as hundreds place indicator and create space accordingly
@@ -46,7 +56,8 @@ def parse_int(int_word):
 
 
 #  Test
-test = ["one", "twenty", "two hundred and forty-six", "seven hundred eighty-three thousand nine hundred and nineteen"]
+test = ["one", "twenty", "fifty-six", "two hundred and forty-six", "fifty-five million six hundred seventy eight thousand two hundred and one", 
+"seven hundred eighty-three thousand nine hundred and nineteen"]
 
 for string_of_number in test:
     print(parse_int(string_of_number))
