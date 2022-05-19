01fb4c7ee390ee4b50d08dec55abde2f47c2a0af
Richard Cha <charichard09@gmail.com>
2022-05-19 12:58:22 -0700

    move hundreds p.v. code outside scope of for loop

--- codeWarsPrograms/string_to_int_2.py
+++ codeWarsPrograms/string_to_int_2.py
@@ -5,8 +5,8 @@ def parse_int(int_word):
     print(f"The number we are parsing is: {int_word}")
 
     #  Remove any hyphen characters and "and" string and replace with ' '
-    int_word = int_word.remove('-')
-    int_word = int_word.remove("and")
+    int_word = int_word.replace("-", " ")
+    int_word = int_word.replace(" and", "")
 
     #  Assign number to be stored and returned, and string broken into a list, to variables
     number = 0
@@ -16,6 +16,7 @@ def parse_int(int_word):
     thousand_place_value_split = []
     thousand_place_value = 0
     thousand_calculated_flag = 0
+    hundreds_place_value_split = []
     hundreds_place_value = 0
     int_word_seperated = int_word.split(" ")
 
@@ -27,13 +28,21 @@ def parse_int(int_word):
     "twenty":20, "thirty":30, "forty":40, "fifty":50, "sixty":60, "seventy":70, "eighty":80, "ninety":90}
     place_value = {"hundred":100, "thousand":1000, "million":1000000}
 
-    print(int_word_seperated)
+    print(f"This is the number seperated in a list: {int_word_seperated}")
     
     for word in int_word_seperated:
         #  If million is in the string, find the integer at place value million, multiply by 1000000, and store in million_place_value_split
         if "million" in int_word_seperated and not million_calculated_flag:   
             if word != "million":
-                million_place_value_split.append(written_numbers[word])
+                if word in written_numbers:
+                    million_place_value_split.append(written_numbers[word])
+                    continue
+                elif word == "hundred":
+                    million_place_value_split.append("hundred")
+                    continue
+                else:
+                    #  TODO: Find and convert print to actually raise error code
+                    print("ERROR: word not found in either written numbers or place value")
             else:
                 #  add values to get million place value
 
@@ -43,7 +52,6 @@ def parse_int(int_word):
                     million_place_value_split[0] = million_place_value_split[0] * 100
                 
                 #  Add together the hundreds/tens/ones place values then multiply by 1 million
-                print(million_place_value_split)
                 for i in million_place_value_split:
                     million_place_value = million_place_value + i
                 million_place_value = million_place_value * 1000000
@@ -54,27 +62,42 @@ def parse_int(int_word):
         #  Test: If "million" was calculated, word would still start this if loop at the index where "thousand" place value would begin
         if "thousand" in int_word_seperated and not thousand_calculated_flag:
             if word != "thousand":
-                  thousand_place_value_split.append(written_numbers[word])
+                if word in written_numbers:
+                    thousand_place_value_split.append(written_numbers[word])
+                    continue
+                elif word == "hundred":
+                    thousand_place_value_split.append("hundred")
+                    continue
+                else:
+                    print("ERROR: word not found in either written numbers or place value")
             else:
                 if "hundred" in thousand_place_value_split:
                     thousand_place_value_split.remove("hundred")
                     thousand_place_value_split[0] = thousand_place_value_split[0] * 100
                 
-                print(thousand_place_value_split)
                 for j in thousand_place_value_split:
                     thousand_place_value = thousand_place_value + j
                 thousand_place_value = thousand_place_value * 1000
                 thousand_calculated_flag = 1
                 continue
         
-        #  find the integer at the ones, tens, hundreds place value, then add million_place_value + thousand_place_value + this result to
+        #  TODO: find the integer at the ones, tens, hundreds place value, then add million_place_value + thousand_place_value + this result to
         #  get final number
-        if "hundred" in int_word_seperated:
-            int_word_seperated.remove("hundred")
-            hundreds_place_value = written_numbers[word] * 100
+        if word == "hundred":
+            hundreds_place_value_split.append("hundred")
+            continue
+        elif word in written_numbers:
+            hundreds_place_value_split.append(written_numbers[word])
             continue
         
-        hundreds_place_value = hundreds_place_value + written_numbers[word]
+    if "hundred" in hundreds_place_value_split:
+        hundreds_place_value_split.remove("hundred")
+        hundreds_place_value[0] = hundreds_place_value_split[0] * 100
+        
+    #  Check hundreds_place_value_split
+    print(hundreds_place_value_split)
+    for k in hundreds_place_value_split:
+        hundreds_place_value = k + hundreds_place_value
     
     number = million_place_value + thousand_place_value + hundreds_place_value
 
