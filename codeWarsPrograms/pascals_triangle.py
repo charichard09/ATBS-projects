eb0332e32d6e188f79fb049570aaa2ae9c6d4a05
Richard Cha <charichard09@gmail.com>
2022-05-26 03:54:26 -0700

    begin pascals triangle pseudo code

---
+++ codeWarsPrograms/pascals_triangle.py
@@ -0,0 +1,13 @@
+#  Program that mimics Pascal's Triangle formula. In Pascal's Triangle, each cell is the sum of the number, or numbers, directly above it.
+#  Given an integer for number of rows (1 <= numRows <= 30), return the values for each cell in each row from top to bottom  
+#  (i.e. numRows = 5, return [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]) 
+
+
+def pascals_triangle(numRows):
+    #  TODO:
+    
+    return
+
+
+number_rows = input("Please enter between 1 and 30 how many rows to calculate for Pascal's Triangle:\n")
+print(pascals_triangle(number_rows))
