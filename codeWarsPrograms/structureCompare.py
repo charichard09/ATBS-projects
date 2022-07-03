41646a27708b0a166c0d5185f74d2521214acbf1
Richard Cha <charichard09@gmail.com>
2022-07-03 09:08:18 -0700

    return false if nested items are dissimilar

--- codeWarsPrograms/structureCompare.py
+++ codeWarsPrograms/structureCompare.py
@@ -9,8 +9,19 @@ def same_structure_as(original,other):
         return bool(False)
 
     #  To avoid comparing values, use type() to convert the values to their respective data types
+    #  TODO: Fix [1,[1,1]] not same as [2,[2]]
     for count, structure in enumerate(original):
         if type(other[count]) == type(structure):
+            print(f"original: {structure} other: {other[count]}")
+
+            #  Using isinstance() function to check if either structure or other[count] are list, if True
+            #  need to check nested values against eachother
+            if isinstance(structure, list):
+                for count_nested, structure_nested in enumerate(structure):
+                    if type(structure_nested) == type(other[count][count_nested]):
+                        continue
+                    else:
+                        return bool(False)
             continue
         else:
             #  Python normally treats returning False as a function failure and ends the function. 
@@ -19,9 +30,11 @@ def same_structure_as(original,other):
         
     return bool(True)
 
+#  TODO: Need to fix input() inputing a string of list argument. We want the actual list itself. 
 original_argument = input("Please enter the original argument: \n")
 new_argument = input("Please enter the new argument to compare against the original argument: \n")
 
 print(f"\nIt is {same_structure_as(original_argument, new_argument)} that the original argument and new argument are equal.")
 
 #  Test: [1,[1,1]],[2,[2,2]] = True, [1,[1,1]],[[2,2],2] = False, [ [ [ ], [ ] ] ], [ [ 1, 1 ] ] = False
+#  Test 2: [1,[1,1]], [2,[2]] = False
