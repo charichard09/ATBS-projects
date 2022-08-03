12692e5a0736cd4a22d4672de3a9999067b57d62
Richard Cha <charichard09@gmail.com>
2022-08-03 15:47:46 -0700

    begin coding path finder 1

---
+++ codeWarsPrograms/path_finder/path_finder_1.py
@@ -0,0 +1,18 @@
+#  Given you are at position [0][0] in an NxN structure, you can only move in cardinal directions North, East, West, South,
+#  return True if you can reach position [N-1][N-1], return false if the path is blocked.
+#  empty positions are marked with a '.'
+#  occupied positions are marked with a 'W'
+
+def path_finder(maze):
+    return
+
+
+#  Test path_finder()
+test = "\n".join([
+          ".W...",
+          ".W...",
+          ".W.W.",
+          "...W.",
+          "...W."])
+
+print(f"Path to the end is {bool(path_finder(test))}")
