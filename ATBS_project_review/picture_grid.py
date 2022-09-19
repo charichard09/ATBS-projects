b51e55715e1715d6f1e392d3b95c501b59f9f63c
Richard Cha <charichard09@gmail.com>
2022-09-19 12:14:54 -0700

    begin picture_grid

---
+++ ATBS_project_review/picture_grid.py
@@ -0,0 +1,17 @@
+#  Given a previouse list of lists image,
+#  print the image below, where x is the length of each nested list, and y is how many lists exist.
+#  ..00.00..
+#  .0000000.
+#  .0000000.
+#  ..00000..
+#  ...000...
+#  ....0....
+
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+print(grid)
