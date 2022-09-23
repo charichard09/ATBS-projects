e9ccb7f0e3da2eeca553e694061441a94cee0bda
Richard Cha <charichard09@gmail.com>
2022-09-23 12:21:03 -0700

    na

--- ATBS_project_review/display_inventory.py
+++ ATBS_project_review/display_inventory.py
@@ -1,6 +1,7 @@
 #  Create a function that when passed a dictionary containing a video game inventory, will print all its
 #  items and a total number of items. The keys will be strings of the items, and the values will be how many.
 #  Write a second function that will take another inventory as "loot" and add it to the "inventory".
+#  TODO: Add a drop chance % table on top of the existing loot amount
 #  Date: 09-23-22  Dev: Richard Cha
 
 from random import randint
