9924bdf3cf6dc804fdb466459e9d1cc238fe9cda
Richard Cha <charichard09@gmail.com>
2022-09-23 11:31:38 -0700

    start program outline

---
+++ ATBS_project_review/display_inventory.py
@@ -0,0 +1,10 @@
+#  Create a function that when passed a dictionary containing a video game inventory, will print all its
+# items and a total number of items. The keys will be strings of the items, and the values will be how many.
+#  Date: 09-23-22  Dev: Richard Cha
+
+def display_inventory(inventory):
+    pass
+
+stuff = {"rope": 1, "torch": 6, "gold coin": 42, "dagger": 1, "arrow": 12}
+
+display_inventory(stuff)
